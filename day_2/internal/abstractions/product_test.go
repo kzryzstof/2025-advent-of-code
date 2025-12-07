21aa69b382cbe3766075b43126833dca482c26bf
kzryzstof <kzryzstof@proton.me>
2025-12-06 20:28:39 -0500

    refactor product test to use NewProduct function for instantiation

--- day_2/internal/abstractions/product_test.go
+++ day_2/internal/abstractions/product_test.go
@@ -78,7 +78,7 @@ func TestProduct_IsValid(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			product, _ := New(tt.productId)
+			product, _ := NewProduct(tt.productId)
 			actualIsValid := product.IsValid()
 
 			if actualIsValid != tt.expectedIsValid {
